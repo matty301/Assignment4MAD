49ca00e32b718e58f5e183607d6d38ed66fdc2f7
Mateen <mateenitsu@gmail.com>
2018-01-13 08:25:07 +0500

    Initial commit

---
+++ app/src/main/java/com/example/mateen/assignment4mad/Event.java
@@ -0,0 +1,18 @@
+package com.example.mateen.assignment4mad;
+
+/**
+ * Created by Mateen on 1/13/2018.
+ */
+
+public class Event {
+
+    int percentage;
+
+    public Event(int percentage) {
+        this.percentage = percentage;
+    }
+
+    public int getPercentage() {
+        return percentage;
+    }
+}
